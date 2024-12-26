a848ac536aaea30f1e54e458e7b1a8d4e4b7ff7e
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-26 22:15:53 +0300

    Add test wrapper to fetch and log player data for testing the sdk

---
+++ testWrapper/index.ts
@@ -0,0 +1,7 @@
+import { Player } from "./../src/interfaces";
+import { getCombinedData } from "../src/index";
+
+const PLAYER_ID = 351; // Erling Haaland
+
+const player: Player | null = await getCombinedData(PLAYER_ID);
+console.log(JSON.stringify(player, null, 2));
