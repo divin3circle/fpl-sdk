4a513f6bf7609d366a5cec1946a14b886c3e290e
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-26 22:36:01 +0300

    Refactor test script to use tsup for building and add main function for execution

--- testWrapper/index.ts
+++ testWrapper/index.ts
@@ -3,5 +3,7 @@ import { getCombinedData } from "../src/index";
 
 const PLAYER_ID = 351; // Erling Haaland
 
-const player: Player | null = await getCombinedData(PLAYER_ID);
-console.log(JSON.stringify(player, null, 2));
+const main = async () => {
+  const player: Player | null = await getCombinedData(PLAYER_ID);
+  console.log(JSON.stringify(player, null, 2));
+};
